05e8042370e584650ed2560671e6342d497c7379
=sameh mohamed <samehznaty@gmail.com>
2023-01-29 12:28:39 +0200

    new update

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,7 +1,9 @@
+import { Ingredient } from './../shopping-list/ingredient.model';
 export class Recipe {
   constructor(
     public name: string,
     public description: string,
-    public imagePath: string
+    public imagePath: string,
+    public ingredient: Ingredient[]
   ) {}
 }
